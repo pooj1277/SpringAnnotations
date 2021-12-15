d19ff42d7c2ba6acdc9566b3041c403454f4f44f
pooj1277 <pooja.sd3@gmail.com>
2021-12-14 22:56:21 -0800

    [Added] Simple spring app

---
+++ src/main/java/com/bridgelabz/firstSpringApp/FirstSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.firstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+		System.out.println("running code");
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/firstSpringApp/HelloRestController.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.firstSpringApp;
+import org.springframework.web.bind.annotation.*;
+import com.bridgelabz.firstSpringApp.User;
+
+@RestController
+@RequestMapping ("/hello")
+public class HelloRestController {
+
+	@RequestMapping(value = {"", "/", "/home"})
+	public String sayHello() { 
+		return "Hello From BridgeLabz!!!";
+	}
+
+	@RequestMapping(value = ("/query"), method = RequestMethod.GET) 
+	public String sayHello(@RequestParam(value="name") String name) {
+		return "Hello " +name + "!";
+	}
+
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello" + name + "!";
+	}
+
+	@PostMapping("/post") 
+	public String sayHello(@RequestBody User user) {
+		return "Hello" +user.getFirstName() + "" + user.getLastName()+ "!";
+	}
+	
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello" + firstName + "" + lastName + "!";
+	}
+}
