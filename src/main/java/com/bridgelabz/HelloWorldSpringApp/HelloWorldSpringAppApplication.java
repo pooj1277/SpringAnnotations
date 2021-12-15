cb0a5612879cc5c90c0374e94ac10eab99cb43be
pooj1277 <pooja.sd3@gmail.com>
2021-12-14 23:06:30 -0800

    [Added] Spring app to print hello world to console and web page

---
+++ src/main/java/com/bridgelabz/HelloWorldSpringApp/HelloWorldSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.HelloWorldSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringAppApplication.class, args);
+		System.out.println("Hello from Pooja");
+	}
+
+}
